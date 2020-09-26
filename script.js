791c3760750fd64bfacee48a0049bb97d44ab2ad
jullinhomn <70717085+jullinhomn@users.noreply.github.com>
2020-09-26 00:30:00 -0300

    Script.js
    Adicionei restrições aos campos.

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-
 function salvarDados1() {
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let cpf = document.getElementById('cpf').value; // documentos
     let celular = document.getElementById('contato').value;
-    resultado.innerHTML = '';
-    resultado.innerHTML += `<strong>Dados do canditado:</strong> <br> Nome: ${nome}<br>E-mail: ${email}<br>CPF: ${cpf}<br>Celular: ${celular} <br> Curso: <strong>Aperfeiçoamento em Panificação</strong>`;
-    
-    
-}
+    // Restrição de campos obrigatórios 
+        if (nome.length <= 10) {
+            alert('Favor preencher com o Nome Completo.')
+        } else if (email.length <= 7){
+            alert('Favor preencher o E-mail corretamente.')
+        } else if (cpf.length <= 6) {
+            alert('Favor preencher o Cpf corretamente.')            
+        } else if(celular.length <= 7){
+            alert('Favor preencher o Contato corretamente.')
+        } else {
+            resultado.innerHTML = '';
+            resultado.innerHTML += `<strong>Dados do canditado:</strong> <br> Nome: ${nome}<br>E-mail: ${email}<br>CPF: ${cpf}<br>Celular: ${celular} <br> Curso: <strong>Aperfeiçoamento em Panificação</strong>`;
+        }
+    }
 
-function salvarDados2() {
+    function salvarDados2() {
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let cpf = document.getElementById('cpf').value; // documentos
     let celular = document.getElementById('contato').value;
-    resultado.innerHTML = '';
-    resultado.innerHTML += `<strong>Dados do canditado:</strong> <br> Nome: ${nome}<br>E-mail: ${email}<br>CPF: ${cpf}<br>Celular: ${celular} <br> Curso: <strong>Operador de Telemarketing</strong>`;
-    
+    // Restrição de campos obrigatórios 
+        if (nome.length <= 10) {
+            alert('Favor preencher com o Nome Completo.')
+        } else if (email.length <= 7){
+            alert('Favor preencher o E-mail corretamente.')
+        } else if (cpf.length <= 6) {
+            alert('Favor preencher o Cpf corretamente.')            
+        } else if(celular.length <= 7){
+            alert('Favor preencher o Contato corretamente.')
+        } else {
+            resultado.innerHTML = '';
+            resultado.innerHTML += `<strong>Dados do canditado:</strong> <br> Nome: ${nome}<br>E-mail: ${email}<br>CPF: ${cpf}<br>Celular: ${celular} <br> Curso: <strong>Operador de Telemarketing</strong>`;
+    }
 }
+
